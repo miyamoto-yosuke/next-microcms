b6ba9752875a3d9c6da4e54c5833ea677537314c
miyamoto-yosuke <miyamoto.chichi@gmail.com>
2021-02-12 12:42:54 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,46 @@
+import fetch from 'isomorphic-unfetch';
+import Link from 'next/link';
+import React from 'react'
+
+const Home = ({blogs}) => {
+  return (
+    <div>
+      <h2>最新の記事</h2>
+      <div>
+        {blogs.map(blog => (
+          <React.Fragment key={blog.id}>
+            <Link href="/blogs/[id]" as={`blogs/${blog.id}.html`}>
+              <a>
+                <h2>{blog.title}</h2>
+              </a>
+            </Link>
+            {blog.tags.map(tag => (
+              <React.Fragment key={tag.id}>
+                <span>{tag.name}</span>
+              </React.Fragment>
+            ))}
+          </React.Fragment>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const getStaticProps = async () => {
+  const key = {
+    headers: {'X-API-KEY': process.env.API_KEY},
+  };
+  const res = await fetch(
+    `https://miya.microcms.io/api/v1/blogs/`,
+    key,
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      blogs: data.contents,
+    }
+  }
+};
+
+export default Home;
